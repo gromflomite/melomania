873eb3f22ff3a3d5c4a03233b9c7844792f86f34
gromflomite <35708235+gromflomite@users.noreply.github.com>
2020-06-15 13:20:52 +0200

    Modify connection manager. Now the SQLite DB is working properly.

--- src/main/java/model/ConnectionManager.java
+++ src/main/java/model/ConnectionManager.java
@@ -1,22 +1,23 @@
 package model;
 
+import java.io.File;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class ConnectionManager {
+public class ConnectionManager {	
 
-	private final static String CONNECTION_URL = "jdbc:sqlite:/melomania.db";	
-
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
-
-		Connection dbConnection = null;
-
+	public static Connection getConnection() throws Exception {		
+		
 		// Checking if the connector is working properly (added in Maven pom.xml)
-		Class.forName("org.sqlite.JDBC"); 
+		Class.forName("org.sqlite.JDBC");
 
-		// Establish connection
-		dbConnection = DriverManager.getConnection(CONNECTION_URL);
+		// Getting the path to DB file (DB saved in /resources)
+		URL resource = ConnectionManager.class.getResource("melomania.db");
+		String dbPath = new File(resource.toURI()).getAbsolutePath();
+
+		// Calling the DB with obtained path
+		Connection dbConnection = DriverManager.getConnection(String.format("jdbc:sqlite:%s", dbPath));//		
 
 		return dbConnection;
 	}
